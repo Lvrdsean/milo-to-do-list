c0c1e0646f0926b7666d4d4951aeaa0ef563750b
Sean Khan <seanny813@optonline.net>
2024-04-09 15:25:26 -0400

    Added TodoContext

---
+++ context/TodoContext.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const TodoContext = () => {
+	return (
+		<View>
+			<Text>This is Todo Context</Text>
+		</View>
+	);
+};
+
+export default TodoContext;
