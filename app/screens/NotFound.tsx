0534e02d8ad44d8701e0d8fd284072f9186573a4
Sean Khan <seanny813@optonline.net>
2024-04-09 16:04:10 -0400

    404 Error Screen

---
+++ app/screens/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function NotFound() {
+	return (
+		<View>
+			<Text>404 - Not Found</Text>
+		</View>
+	);
+}
