538ac2dd496e1ae7d07855be6f27adeedcae5179
Sean Khan <seanny813@optonline.net>
2024-04-09 16:11:43 -0400

    Added TodoList Functionality

--- context/TodoContext.tsx
+++ context/TodoContext.tsx
@@ -1,11 +1,66 @@
-import { View, Text } from "react-native";
+import React, { createContext, useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const TodoContext = () => {
-	return (
-		<View>
-			<Text>This is Todo Context</Text>
-		</View>
-	);
-};
+interface Task {
+	id: string;
+	title: string;
+	complete: boolean;
+}
 
-export default TodoContext;
+interface TodoList {
+	id: string;
+	title: string;
+	tasks: Task[];
+}
+
+interface TodoContextType {
+	todoLists: TodoList[];
+	addTodoList: (title: string, id: string) => void;
+	deleteTodoList: (id: string) => void;
+	updateTodoListTitle: (id: string, newTitle: string) => void;
+	addTask: (listId: string, taskTitle: string) => void;
+	deleteTask: (listId: string, taskId: string) => void;
+	updateTaskTitle: (listId: string, taskId: string, newTitle: string) => void;
+}
+
+const TodoContext = createContext<TodoContextType>({
+	todoLists: [],
+	addTodoList: () => {},
+	deleteTodoList: () => {},
+	updateTodoListTitle: () => {},
+	addTask: () => {},
+	deleteTask: () => {},
+	updateTaskTitle: () => {},
+});
+
+export const useTodoContext = () => useContext(TodoContext);
+
+interface TodoContextProviderProps {
+	children: React.ReactNode;
+}
+
+export const TodoContextProvider: React.FC<TodoContextProviderProps> = ({
+	children,
+}) => {
+	const [todoLists, setTodoLists] = useState<TodoList[]>([]);
+
+	const addTodoList = (title: string, id: string) => {
+		const newTodoList: TodoList = {
+			id,
+			title,
+			tasks: [],
+		};
+		setTodoLists([...todoLists, newTodoList]);
+	};
+
+	const deleteTodoList = (id: string) => {
+		setTodoLists(todoLists.filter((todoList) => todoList.id !== id));
+	};
+
+	const updateTodoListTitle = (id: string, newTitle: string) => {
+		setTodoLists(
+			todoLists.map((todoList) =>
+				todoList.id === id ? { ...todoList, title: newTitle } : todoList
+			)
+		);
+	};
